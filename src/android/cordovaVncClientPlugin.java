3921012d74d271cc904807a1a77a8ff6d0569dd7
omar maatoug <ommaatoug@gmail.com>
2020-02-10 11:18:02 +0100

    edit plugin id

--- src/android/cordovaVncClientPlugin.java
+++ src/android/cordovaVncClientPlugin.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class cordova-vnc-client-plugin extends CordovaPlugin {
+public class cordovaVncClientPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
